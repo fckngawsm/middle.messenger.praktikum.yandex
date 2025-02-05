9483847f9444ac6b48b1c381ea1b560063c49208
Kirill <marchenko.kiri11@yandex.ru>
2025-02-05 23:47:34 +0300

    feat: add new plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
 import postcssPresetEnv from 'postcss-preset-env';
 
 export default {
   plugins: [
     autoprefixer(),
     postcssPresetEnv({ stage: 1 }),
+    postcssNested()
   ],
 };
