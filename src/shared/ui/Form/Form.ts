4d4c0889a07701ea3704a48e3e8d4c96c317dac6
Kirill <marchenko.kiri11@yandex.ru>
2025-03-01 00:54:38 +0300

    feat: create Form component

--- src/shared/ui/Form/Form.ts
+++ src/shared/ui/Form/Form.ts
@@ -1,5 +1,25 @@
+import { Block } from "../../components/Block";
+
 export default `
     <form class="form" id="{{id}}" autocomplete="off">
         {{> @partial-block }}
     </form>
 `;
+
+interface FormProps {
+  id: number;
+}
+
+export class Form extends Block {
+  constructor(props: FormProps) {
+    super(props);
+  }
+
+  protected render(): string {
+    return `
+            <form class="form" id=${this.props.id} autocomplete="off">
+                ${this.children}
+            </form>
+      `;
+  }
+}
