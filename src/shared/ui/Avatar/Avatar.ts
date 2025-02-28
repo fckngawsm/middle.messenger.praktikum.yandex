6ea1f6e088190134178bf6d2548bc2f7273c2616
Kirill <marchenko.kiri11@yandex.ru>
2025-03-01 00:45:51 +0300

    feat: create avatar component

--- src/shared/ui/Avatar/Avatar.ts
+++ src/shared/ui/Avatar/Avatar.ts
@@ -1,1 +1,19 @@
+import { Block } from "../../components/Block";
+
 export default `<img class="avatar {{class}}" src={{userAvatar}} alt="Аватарка"/>`;
+
+interface AvatarProps {
+  class: string;
+  avatarUrl: string;
+  alt: string;
+}
+export class Avatar extends Block {
+  constructor(props: AvatarProps) {
+    super(props);
+  }
+  protected render(): string {
+    return `
+        <img class='avatar ${this.props.class}' src=${this.props.class} alt=${this.props.alt}/>
+      `;
+  }
+}
