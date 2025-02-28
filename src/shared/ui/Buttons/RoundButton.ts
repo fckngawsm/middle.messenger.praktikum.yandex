f13124d9f042dda2f9fb6180dd254b03d65eddc5
Kirill <marchenko.kiri11@yandex.ru>
2025-03-01 01:12:11 +0300

    feat: create RoundButtonComponent

--- src/shared/ui/Buttons/RoundButton.ts
+++ src/shared/ui/Buttons/RoundButton.ts
@@ -1,3 +1,5 @@
+import { Block } from "../../components/Block";
+
 export default `
     <a id="{{id}}" href="{{href}}" class="button__type-round">
         {{#if linkText}}
@@ -8,3 +10,30 @@ export default `
         {{/if}}
     </a>
 `;
+
+interface RoundButtonProps {
+  attr: {
+    id: number;
+    to: string;
+    linkText?: string;
+    icon?: string;
+    iconAlt?: string;
+  };
+}
+
+export class RoundButton extends Block {
+  constructor(props: RoundButtonProps) {
+    super(props);
+  }
+
+  protected render(): string {
+    const { id, to, linkText, icon, iconAlt } = this.props.attr;
+    return `
+        <a id=${id} href=${to} class="button__type-round">
+            ${linkText && linkText}
+
+            ${icon && `<img src=${icon} alt=${iconAlt} class="link__icon"/>`}
+        </a>
+      `;
+  }
+}
