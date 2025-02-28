fce040ea0da2377d18cab6cdb3e6e034014f6f73
Kirill <marchenko.kiri11@yandex.ru>
2025-03-01 00:56:40 +0300

    feat: create FormWrapper component

--- src/shared/ui/FormWrapper.ts
+++ src/shared/ui/FormWrapper.ts
@@ -1,4 +1,24 @@
+import { Block } from "../components/Block";
+
 export default `
 <div class="form__wrapper {{class}}">
     {{> @partial-block }}
 </div>`;
+
+interface FormWrapperProps {
+  class: string;
+}
+
+export class FormWrapper extends Block {
+  constructor(props: FormWrapperProps) {
+    super(props);
+  }
+
+  protected render(): string {
+    return `
+      <div class="form__wrapper ${this.props.class}">
+            ${this.children}
+        </div>;
+    `;
+  }
+}
