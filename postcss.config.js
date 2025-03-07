bd98b5d992887d9aed1b78c01741a3fb4537ea4b
Kirill <marchenko.kiri11@yandex.ru>
2025-03-07 21:03:27 +0300

    chpore: update lint config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-import autoprefixer from 'autoprefixer';
-import postcssNested from 'postcss-nested';
-import postcssPresetEnv from 'postcss-preset-env';
+import autoprefixer from "autoprefixer";
+import postcssNested from "postcss-nested";
+import postcssPresetEnv from "postcss-preset-env";
 
 export default {
   plugins: [
